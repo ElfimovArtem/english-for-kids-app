aab5d46314c68a3413145bd0cfb6e9e8a07a8a52
Elfimov Artem <elfimov021@yandex.ru>
2020-04-14 19:50:50 +0300

    feat: added some files.

--- English-for-kids/script.js
+++ English-for-kids/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const MENU = document.getElementById('MENU');
+const MENU_ITEM = MENU.querySelectorAll('.header-item-link');
+const CHECKBOX = document.getElementById('CHECKBOX');
+const CARDS = document.querySelectorAll('.main-card');
+
+MENU.addEventListener('click', (event) => {
+    MENU_ITEM.forEach(el => el.classList.remove('active'));
+    event.target['classList'].add('active');
+});
+
+CHECKBOX.addEventListener('click', () => {
+    if (CHECKBOX.checked) {
+        CARDS.forEach(el => el.classList.remove('play-styles'));
+        MENU.classList.remove('on-play-menu');
+    } else {
+        CARDS.forEach(el => el.classList.add('play-styles'));
+        MENU.classList.add('on-play-menu');
+    }
+});
